392483c6e5d2f001f4cb013506039bd768c48b9c
Tiny <453616448@qq.com>
2018-02-28 17:11:06 +0800

    update

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress', 'coverage', 'coveralls'],
+    reporters: ['progress'],
 
 
     // web server port
@@ -78,7 +78,7 @@ module.exports = function(config) {
     plugins: [
       'karma-mocha',
       'karma-chrome-launcher',
-      'karma-webpack',
+      'karma-webpack'
     ],
     customLaunchers: {
         ChromeHeadless: {
